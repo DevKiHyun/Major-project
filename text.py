10f59209dac1220d33bd2867228c9220d652e221
DevKiHyu <gunatenam@naver.com>
2016-09-08 18:57:59 +0900

    Assignment 2

--- text.py
+++ text.py
@@ -1,8 +1,11 @@
+import sys
 for i in range(0, int(raw_input())):
-    A , B = int(raw_input().split())
+    A , B = raw_input().split()
+    A = int(A)
+    B = int(B)
     while A != 0 and B != 0 :
         if A > B :
             A = A % B
         else :
             B = B % A
-    print("gcd =", A+B)
+    print(A+B)
