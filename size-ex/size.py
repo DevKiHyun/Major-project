4c3e0513d85c08d4dd63a566ee8b1ca73aae5c62
DevKiHyu <gunatenam@naver.com>
2016-10-11 15:09:46 +0900

    Assignment4

--- size-ex/size.py
+++ size-ex/size.py
@@ -1,22 +1,20 @@
 import os
 
-global total
-
 def disk_size(path):
     total = 0
-    if os.path.isdir(path) == True:
-        L = os.listdir(path)
-        for i in range(0,len(L)):
-            child = os.path.join(path,L[i])
-            size = os.path.getsize(child)
-            print "{0:<7} {1}".format(size, child)
-            total = total + size
-    print "{0:<7} {1}".format(total, path)
+    if os.path.isfile(path) == True :
+        total = os.path.getsize(path)
+        print "{0:<7} {1}".format(total, path)
+    elif os.path.isdir(path) == True :
+        for child in os.listdir(path) :
+            child = os.path.join(path, child)
+            total = total + disk_size(child)
+        print "{0:<7} {1}".format(total, path)
     return total
 
-path = raw_input()
+path = '/home/namkihyun/Major-project'
 if os.path.exists(path) == True:
-    print '\n'
+    print ""
     print disk_size(path)
 else :
-    print "directory is not exist or is wrong"
+    print "Path is not exist or is wrong"
--- size-ex/test.py
+++ size-ex/test.py
@@ -1,7 +1,20 @@
 import os
 
-path ='/home/namkihyun/Major-project/Quick Selection'
+def disk_size(path):
+    total = 0
+    if os.path.isfile(path) == True :
+        total = os.path.getsize(path)
+        print "{0:<7} {1}".format(total, path)
+    elif os.path.isdir(path) == True :
+        for child in os.listdir(path) :
+            child = os.path.join(path, child)
+            total = total + disk_size(child)
+        print "{0:<7} {1}".format(total, path)
+    return total
 
-total = os.path.getsize(path)
-
-print total
+path = '/home/namkihyun/Major-project/size-ex'
+if os.path.exists(path) == True:
+    print ""
+    print disk_size(path)
+else :
+    print "Path is not exist or is wrong"
